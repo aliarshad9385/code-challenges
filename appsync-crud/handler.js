dc1840e841d9ded56b7dbfe4e44ca9bcf9d0f023
aliarshad9385 <135148542+aliarshad9385@users.noreply.github.com>
2025-03-05 18:49:29 -0500

    feat:handler

---
+++ appsync-crud/handler.js
@@ -0,0 +1,10 @@
+// handler.js
+
+module.exports.hello = async (event) => {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: "Hello from Serverless!"
+      })
+    };
+  };
